40ea0508f51f61e52f978b3dd378a5201bbad901
gabriel-miranda <gabmirdev@gmail.com>
2018-01-12 19:15:00 -0300

    AQA-2: Fix warnings in tests

--- src/www/__tests__/header.test.js
+++ src/www/__tests__/header.test.js
@@ -6,9 +6,11 @@ import renderer from 'react-test-renderer';
 
 import Header from '../components/Header';
 
+const dummyFn = () => {};
+
 describe('Header test with enzyme', () => {
   it('should show header title correctly"', () => {
-    const header = shallow(<Header />);
+    const header = shallow(<Header handleSearch={dummyFn} />);
 
     expect(header.find('h1').text()).toEqual('Quotes app');
   });
@@ -16,7 +18,7 @@ describe('Header test with enzyme', () => {
 
 describe('Header test with Snapshot Testing', () => {
   it('should show header title correctly', () => {
-    const component = renderer.create(<Header />);
+    const component = renderer.create(<Header handleSearch={dummyFn} />);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
