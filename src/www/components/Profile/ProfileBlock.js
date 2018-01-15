1f428f097de87b139b72b0c5d6c2b36bb2d52780
gabriel-miranda <gabmirdev@gmail.com>
2018-01-15 06:55:57 -0300

    AQA-3: Added the needed inputs for the values in metadata

--- src/www/components/Profile/ProfileBlock.js
+++ src/www/components/Profile/ProfileBlock.js
@@ -2,8 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const FIELD_STYLES = {
-  margin: '20px 0',
   flexBasis: '50%',
+  padding: '20px 10px 20px 0',
+};
+
+const BIG_FIELD_STYLE = {
+  flexBasis: '100%',
+  margin: '20px 0',
 };
 
 const FIELD_TITLE_STYLES = {
@@ -21,8 +26,40 @@ const FIELD_VALUE_STYLES = {
   verticalAlign: 'top',
 };
 
+const TEXTAREA_STYLEs = {
+  resize: 'vertical',
+};
+
 const ProfileBlock = ({block}) => {
   switch (block.title) {
+    case 'bio':
+      return (
+        <div style={BIG_FIELD_STYLE}>
+          <div style={FIELD_TITLE_STYLES}>{block.title}</div>
+          <textarea
+            className="form-control"
+            type="date"
+            maxLength="500"
+            style={TEXTAREA_STYLEs}
+          >
+            {block.value}
+          </textarea>
+        </div>
+      );
+    case 'birthdate':
+      return (
+        <div style={FIELD_STYLES}>
+          <div style={FIELD_TITLE_STYLES}>{block.title}</div>
+          <input className="form-control" type="date" value={block.value} />
+        </div>
+      );
+    case 'favorite_color':
+      return (
+        <div style={FIELD_STYLES}>
+          <div style={FIELD_TITLE_STYLES}>{block.title}</div>
+          <input className="form-control" type="color" value={block.value} />
+        </div>
+      );
     default:
       return (
         <div style={FIELD_STYLES}>
